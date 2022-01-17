78c26ec05091d536f61d49e4ed5941121811934b
Naser Alhawamdeh <nalhawamdeh@ucdavis.edu>
2022-01-17 08:55:07 +0400

    for 4 and 3 options chosen completed

--- Develop/script.js
+++ Develop/script.js
@@ -17,7 +17,7 @@ var toUpperCase = function() {
   return toUpperCase();
 };
 
-characters = chars(toUpperCase);
+uppercaseLetters = letters(toUpperCase);
 
 var generate = document.querySelector("#generate");
 
@@ -27,3 +27,49 @@ generate.addEventListener("click", function () {
 });
 
 
+// Function to generate the password
+
+function generatePassword (){
+  chooseNumber = parseInt(prompt("How many characters do you want your password to be? Choose between 8 and 128"));
+
+  if (!chooseNumber) {
+    alert("This needs a value! Try again.");
+  } else if (chooseNumber < 8 || chooseNumber > 128) {
+    chooseNumber = parseInt("You must enter a value between 8-128 to proceed");
+
+  }else {
+    confirmNumber = confirm("Do you want your password to have numbers?");
+    confirmCharacter = confirm("Do you want your password to have special characters?");
+    confirmUppercase = confirm("Do you want your password to have uppercase letters?");
+    confirmLowercase = confirm("Do you want your password to have lowercase letters?");
+  };
+
+  // If no choices are made for all of the 4 prompts
+  if (!confirmNumber && !confirmCharacter && !confirmUppercase && !confirmLowercase) {
+    options = alert("You must choose at least one of the options!");
+  }
+
+  // For 4 options chosen
+  else if (confirmNumber && confirmCharacter && confirmUppercase && confirmLowercase) {
+    options = chars.concat(num, letters, uppercaseLetters);
+  }
+
+  // For 3 options chosen
+  else if (confirmNumber && confirmCharacter && confirmUppercase) {
+    options = chars.concat(num, uppercaseLetters);
+  }
+  else if (confirmNumber && confirmCharacter && confirmLowercase) {
+    options = chars.concat(num, letters);
+  }
+  else if (confirmCharacter && confirmUppercase && confirmLowercase) {
+    options = chars.concat(uppercaseLetters, letters);
+  }
+  else if (confirmNumber && confirmLowercase && confirmUppercase) {
+    options = num.concat(letters, uppercaseLetters);
+  }
+
+  // For 2 options chosen 
+else if (confirmNumber && confirmLowercase) {
+
+}
+}
